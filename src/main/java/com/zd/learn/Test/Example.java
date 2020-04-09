a12c49a1e9666bad906b8a286b62577451ba73fb
fullstack <1501613873@qq.com>
2020-04-09 13:06:27 +0800

    mybatis 逆向工程

---
+++ src/main/java/com/zd/learn/Test/Example.java
@@ -0,0 +1,20 @@
+package com.zd.learn.Test;
+
+import com.taotao.entity.TbItemCat;
+import com.taotao.mapper.TbItemCatMapper;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author zd
+ * @Date 2020/4/9 12:52
+ * @Version 1.0
+ * 描述:
+ */
+public class Example {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("springmvc.xml");
+        TbItemCatMapper tbItemCatMapper = context.getBean("tbItemCatMapper", TbItemCatMapper.class);
+        TbItemCat tbItemCat = tbItemCatMapper.selectByPrimaryKey((long) 1);
+        System.out.println(tbItemCat);
+    }
+}
---
+++ src/main/java/com/zd/learn/generator/Generator.java
@@ -0,0 +1,34 @@
+package com.zd.learn.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator {
+	public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+		//加载配置文件
+		File file = new File("src/main/resources/generatorConfig.xml") ;
+		//使用列表来存储日志信息
+		List<String> warnings = new ArrayList<>();
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		//解析配置文件信息
+		Configuration config = cp.parseConfiguration(file);
+
+		DefaultShellCallback callBack = new DefaultShellCallback(true);
+		
+		//逆向工程的核心类
+		MyBatisGenerator generator = new MyBatisGenerator(config, callBack,warnings  );
+		generator.generate(null);
+		//打印日志信息
+		System.out.println(warnings.toString());
+	}
+}
